8d76e2cd49fba0b9f489096d737389f2fe728e7c
KasperBahrentz <kasperbahrentz@gmail.com>
2025-05-04 20:24:00 +0200

    API now fetches 24 images

--- server/api/unsplash.ts
+++ server/api/unsplash.ts
@@ -12,7 +12,7 @@ export default defineEventHandler(async (event) => {
     const response = await $fetch('https://api.unsplash.com/search/photos', {
       params: {
         query,
-        per_page: 16,
+        per_page: 24,
       },
       headers: {
         Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
